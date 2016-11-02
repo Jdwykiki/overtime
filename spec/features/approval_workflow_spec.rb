ad49ffbafc299be921e9c9227d599139223a69aa
Justin Wykstra <justinwykstra@gmail.com>
2016-11-02 13:04:28 -0600

    Updated views to hide edit button once a post is approved

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -30,15 +30,15 @@ describe 'navigate' do
     end
 
     it 'should not be editable by the post creator if status is approved' do
-    logout(:user)
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
-    @post.update(user_id: user.id, status: 'approved')
+      @post.update(user_id: user.id, status: 'approved')
 
-    visit edit_post_path(@post)
+      visit edit_post_path(@post)
 
-    expect(current_path).to eq(root_path)
+      expect(current_path).to eq(root_path)
     end
 	end
 end
